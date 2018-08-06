90115009a6090faa50001f417843f4642ea35dda
yutian1012 <yutian1012@126.com>
2018-08-06 12:42:12 +0800

    u

---
+++ beandemo/src/main/java/com/sjq/bean/beanFactoryPostProcessor/Student.java
@@ -0,0 +1,18 @@
+package com.sjq.bean.beanFactoryPostProcessor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	private String name;
+	
+	public Student() {
+		System.out.println("【student类】构造函数");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
